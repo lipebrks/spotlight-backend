9588f8bed53a9da2e8d2a116fa34b572506c901f
Filipe Totaro Bruekers <lipebrks@gmail.com>
2025-06-24 21:40:39 -0300

    Atualização do allowedOrigins para incluir o frontend do GitHub Pages e a URL do Render

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const allowedOrigins = [
     'https://lipebrks.github.io/spotlight',
     'http://localhost:8000', 
     'http://localhost:5500', 
-    'http://127.0.0.1:5500' 
+    'http://127.0.0.1:5500',
+    'https://spotlight-backend-z6xo.onrender.com'
 ];
 
 server.use((req, res, next) => {
